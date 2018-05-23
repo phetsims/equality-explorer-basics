222930b86671b7f988267f264b1173a163966659
Chris Malley <cmalley@pixelzoom.com>
2018-05-23 12:13:11 -0600

    create screen icon, #3
    Signed-off-by: Chris Malley <cmalley@pixelzoom.com>

--- js/lab/LabScreen.js
+++ js/lab/LabScreen.js
@@ -12,10 +12,18 @@ define( function( require ) {
   var equalityExplorerBasics = require( 'EQUALITY_EXPLORER_BASICS/equalityExplorerBasics' );
   var EqualityExplorerColors = require( 'EQUALITY_EXPLORER/common/EqualityExplorerColors' );
   var EqualityExplorerScreen = require( 'EQUALITY_EXPLORER/common/EqualityExplorerScreen' );
+  var HBox = require( 'SCENERY/nodes/HBox' );
+  var Image = require( 'SCENERY/nodes/Image' );
   var inherit = require( 'PHET_CORE/inherit' );
-  var Property = require( 'AXON/Property' );
   var LabModel = require( 'EQUALITY_EXPLORER_BASICS/lab/model/LabModel' );
   var LabScreenView = require( 'EQUALITY_EXPLORER_BASICS/lab/view/LabScreenView' );
+  var NumberPicker = require( 'SCENERY_PHET/NumberPicker' );
+  var Property = require( 'AXON/Property' );
+  var Range = require( 'DOT/Range' );
+  var ScreenIcon = require( 'JOIST/ScreenIcon' );
+
+  // images
+  var squareImage = require( 'image!EQUALITY_EXPLORER/square.png' );
 
   // strings
   var screenLabString = require( 'string!EQUALITY_EXPLORER_BASICS/screen.lab' );
@@ -28,8 +36,8 @@ define( function( require ) {
 
     options = _.extend( {
       name: screenLabString,
-      backgroundColorProperty: new Property( EqualityExplorerColors.BASICS_SCREEN_BACKGROUND )
-      //TODO homeScreenIcon
+      backgroundColorProperty: new Property( EqualityExplorerColors.BASICS_SCREEN_BACKGROUND ),
+      homeScreenIcon: createScreenIcon()
     }, options );
 
     EqualityExplorerScreen.call( this,
@@ -40,6 +48,28 @@ define( function( require ) {
   }
 
   equalityExplorerBasics.register( 'LabScreen', LabScreen );
+
+  // Creates the icon for this screen, a shape and a picker
+  function createScreenIcon() {
+
+    var squareNode = new Image( squareImage, {
+      scale: 0.75
+    } );
+
+    var pickerNode = new NumberPicker( new Property( 7 ), new Property( new Range( 0, 10 ) ), {
+      color: 'black'
+    } );
+
+    var iconNode = new HBox( {
+      spacing: 10,
+      children: [ squareNode, pickerNode ]
+    } );
+
+    return new ScreenIcon( iconNode, {
+      maxIconWidthProportion: 0.75,
+      fill: EqualityExplorerColors.VARIABLES_SCREEN_BACKGROUND
+    } );
+  }
 
   return inherit( EqualityExplorerScreen, LabScreen );
 } );
