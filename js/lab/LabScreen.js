2683088e4d5915dcaea00121ba02bde03474e721
Marla Schulz <marlaa.schulz@gmail.com>
2022-06-28 16:08:18 -0600

    Move NumberPicker to sun, see: https://github.com/phetsims/sun/issues/436

--- js/lab/LabScreen.js
+++ js/lab/LabScreen.js
@@ -14,7 +14,7 @@ import EqualityExplorerScreen from '../../../equality-explorer/js/common/Equalit
 import ScreenIcon from '../../../joist/js/ScreenIcon.js';
 import merge from '../../../phet-core/js/merge.js';
 import MathSymbols from '../../../scenery-phet/js/MathSymbols.js';
-import NumberPicker from '../../../scenery-phet/js/NumberPicker.js';
+import NumberPicker from '../../../sun/js/NumberPicker.js';
 import PhetFont from '../../../scenery-phet/js/PhetFont.js';
 import { HBox } from '../../../scenery/js/imports.js';
 import { Image } from '../../../scenery/js/imports.js';
