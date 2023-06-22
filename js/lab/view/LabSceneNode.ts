8e2598d9701765eb36c6d7784c92099ed95d7a61
Michael Kauzmann <michael.kauzmann@colorado.edu>
2023-06-22 17:32:14 -0600

    convert to Disposable.assertNotDisposable(), https://github.com/phetsims/axon/issues/436

--- js/lab/view/LabSceneNode.ts
+++ js/lab/view/LabSceneNode.ts
@@ -7,6 +7,7 @@
  * @author Chris Malley (PixelZoom, Inc.)
  */
 
+import Disposable from '../../../../axon/js/Disposable.js';
 import BooleanProperty from '../../../../axon/js/BooleanProperty.js';
 import Property from '../../../../axon/js/Property.js';
 import Bounds2 from '../../../../dot/js/Bounds2.js';
@@ -89,7 +90,7 @@ export default class LabSceneNode extends BasicsSceneNode {
   }
 
   public override dispose(): void {
-    assert && assert( false, 'dispose is not supported, exists for the lifetime of the sim' );
+    Disposable.assertNotDisposable();
     super.dispose();
   }
 
